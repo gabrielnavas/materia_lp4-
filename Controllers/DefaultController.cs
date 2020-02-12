b6bd5daca8107c2c7d271ffd561e1e810b73f166
gabrielnavas <gabrielnavas@email.com>
2020-02-11 21:20:03 -0300

    Add files via upload

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWord.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Logar()
+        {
+            ViewBag.Msg = "Dados Inválidos";
+
+            //exemplo, se eu quiser ir para outro lugar, vai para Index
+            return View("Index");
+        }
+
+
+    }
+}
